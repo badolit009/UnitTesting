9d32289d74f6578b9ca99ab7bae65bdecbe129f5
badolit009 <badolit009@gmail.com>
2014-10-31 23:56:25 +0600

    Initial BankAccount Uploaded

---
+++ BankAccountTest/BankAccountTest/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankAccountTest
+{
+    public class Account
+    {
+        public string AccountNo { get; set; }
+        public string CustomerName { get; set; }
+        public double Balance { get; set; }
+        public Account(string accountNo, string customerName):this()
+        {
+            AccountNo = accountNo;
+            CustomerName = customerName;
+        }
+
+        public Account()
+        {
+        }
+        public double Deposit(double amount)
+        {
+           return Balance+=amount;
+        }
+
+        public double WithDraw(double amount)
+        {
+            return Balance-=amount;
+        }
+    }
+}
---
+++ BankAccountTest/BankAccountUnitTest.test/AcccountUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccountTest;
+using NUnit.Framework;
+
+namespace BankAccountUnitTest.test
+{
+    [TestFixture]
+    class AcccountUnitTest
+    {
+        private Account anAccount;
+        [SetUp]
+        public void Start()
+        {
+            
+        }
+        [Test]
+        public void InitialBalance()
+        {
+            anAccount = new Account("009","Badol");
+            Assert.AreEqual(0,anAccount.Balance);
+        }
+        [Test]
+        public void BalanceDeposit()
+        {
+            anAccount = new Account();
+            Assert.AreEqual(100,anAccount.Deposit(100));
+        }
+        [Test]
+        public void BalanceWithdraw()
+        {
+            anAccount = new Account();
+            Assert.AreEqual(-50,anAccount.WithDraw(50));
+        }
+
+        [TearDown]
+        public void End()
+        {
+            
+        }
+
+    }
+}
